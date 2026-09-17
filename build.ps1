cd5ebace75dfc68b801461a2da1cdfd3641fcb8a
David Kantun <dkantun@gmail.com>
2026-09-16 22:15:13 -0600

    PGO

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ $vsRoot = 'C:\Program Files\Microsoft Visual Studio\18\Community'
 $env:PATH = "$vsRoot\VC\Tools\Llvm\x64\bin;" + $env:PATH
 Push-Location $PSScriptRoot
 try {
-    cmake --preset win-amd64-release "-DREXSDK_DIR=$sdkRoot" -DSW2_PGO=OFF
+    cmake --preset win-amd64-release "-DREXSDK_DIR=$sdkRoot" -DSW2_PGO=AUTO
     if ($LASTEXITCODE -ne 0) { throw 'CMake configuration failed.' }
     cmake --build --preset win-amd64-release -j 6
     if ($LASTEXITCODE -ne 0) { throw 'Build failed.' }
--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 sdk_root=$(dirname -- "$script_dir")
 
 cmake --preset linux-amd64-release -S "$script_dir" \
-    "-DREXSDK_DIR=$sdk_root" -DSW2_PGO=OFF
+    "-DREXSDK_DIR=$sdk_root" -DSW2_PGO=AUTO
 cmake --build --preset linux-amd64-release -j 6
